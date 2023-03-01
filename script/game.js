b3d24e31330d29b4aa54c147b8179fce1eb0a6d7
Carlos Alexandre Carmona da Silva <carlosalexandress340@gmail.com>
2023-03-01 12:50:28 -0400

    Base Feita
    Base dos codigos feita, porem, não imprementada.

---
+++ script/game.js
@@ -0,0 +1,28 @@
+/*Variaveis de controle do jogo*/
+var qtdPerguntas = 0;
+var perguntasFeitas = [];
+
+//perguntas do jogo
+const pergunta = [
+    {
+        pergunta: "Qual dessas linguagens não é considerada uma linguagem de programação?  ",
+        respostas: ["PHP", "Javascript", "C++", "HTML"],
+        correta: "resp3"
+    },
+    {
+        pergunta:"Em que ano o Brasil foi descoberto?",
+        respostas:["1498", "1500", "1375", "1828"],
+        correta: "resp1"
+    },
+    {
+        pergunta:"Oque significa HTML?",
+        respostas: ["Hyper Tonto Maluco Legal", "Hyper Text Markup Language", "Hyper Trade More Language", "Hyper Text Mark Lang"],
+        correta: "resp1"
+    },
+    {
+        pergunta: "Quais dessas linguagens é considerada uma linguagem de marcação?",
+        respostas: ["HTML", "Javascript", "C++", "PHP"],
+        correta: 'resp0'
+    }
+
+];
